e7c4b5980c55e9ce40417105f4ada971e8176215
markvk14 <34057780+markvk14@users.noreply.github.com>
2021-03-22 14:16:12 +0100

    Revert "deleted unnecessary files"
    This reverts commit fb71dfbcccd8a4f5d9e0ee487ec54dd4b91835a7.

--- src/main/java/com/project/lingo/Presentation/controller/SpelerRestController.java
+++ src/main/java/com/project/lingo/Presentation/controller/SpelerRestController.java
@@ -2,6 +2,8 @@ package com.project.lingo.Presentation.controller;
 
 import com.project.lingo.Application.SpelerService;
 import com.project.lingo.Domain.Speler;
+import com.project.lingo.Presentation.dto.SpelerDto;
+import com.project.lingo.Presentation.util.GenericResponse;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,4 +19,12 @@ public class SpelerRestController {
 
     @Autowired
     private SpelerService spelerService;
+
+    @PostMapping("/speler/registreren")
+    public GenericResponse registerUserAccount(@Valid final SpelerDto accountDto, final HttpServletRequest request) {
+        LOGGER.debug("Registering user account with information: {}", accountDto);
+
+        final Speler registered = spelerService.registreerNieuwAccount(accountDto);
+        return new GenericResponse("success");
+    }
 }
